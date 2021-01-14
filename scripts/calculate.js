2d081f3aaab39c3d22a74b8526141c59512a11fe
Parmashawn <70422209+Parmashawn@users.noreply.github.com>
2021-01-14 14:22:14 -0600

    Updated and uploaded what i have so far
    - got the dynamic table for the calculator functional
    - calculator has no funciton yet

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -1,1 +1,26 @@
 let fish_list = []
+
+function addRow() {
+          
+    var fish_amount = document.getElementById("fish_amount");
+    var fish_type = document.getElementById("fish_type");
+    var fish_size = document.getElementById("fish_size");
+	var table = document.getElementById("fish_table");
+ 
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+ 
+    row.insertCell(0).innerHTML= '<i class="fas fa-trash-alt" onClick="deleteRow(this)"></i>';
+    row.insertCell(1).innerHTML= fish_amount.value;
+    row.insertCell(2).innerHTML= fish_type.value;
+	row.insertCell(3).innerHTML= fish_size.value;
+ 
+}
+
+function deleteRow(obj) {
+      
+    var index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("fish_table");
+    table.deleteRow(index);
+    
+}
