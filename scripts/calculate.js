81aef64917ec315b8d734f87e1da75dad17075a7
Parmashawn <70422209+Parmashawn@users.noreply.github.com>
2021-01-17 14:22:37 -0600

    Minor fixes and reset button

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -146,15 +146,13 @@ function generatelink() {
 	if (link.slice(0, -1) == window.location.href.split('#')[0]) {document.getElementById("share").value = "...";}
 	else {document.getElementById("share").value = link.slice(0, -1);}
 	
-
-
 }
 
 function load() {
 	var hashParams = window.location.hash.substr(1).split('&'); // substr(1) to remove the `#`
 		for(var i = 0; i < hashParams.length; i++){
 			var p = hashParams[i].split('=');
-			document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
+			document.getElementById(p[0]).value = decodeURIComponent(p[1]);
 		}
 		calculate();
 }
